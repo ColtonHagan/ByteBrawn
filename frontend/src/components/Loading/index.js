60913a8307a5a41fa55532c2d86ed54bfd57c714
Colton <43594936+ColtonHagan@users.noreply.github.com>
2025-04-01 15:49:29 -0700

    updated loading message
    updated loading message to clarify wait and reason

--- frontend/src/components/Loading/index.js
+++ frontend/src/components/Loading/index.js
@@ -8,7 +8,11 @@ const Loading = () => {
     return (
         <div className="spinner-container" role="alert" aria-live="polite">
             <div className="spinner" aria-hidden="true"> </div>
-            <p className="spinner-message">Loading...</p>
+            <p className="spinner-message">
+                Loading...<br>
+                This may take up to 30 seconds<br>
+                Due to free-tier API hosting backend
+            </p>
         </div>
     );
 };
