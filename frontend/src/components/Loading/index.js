e13867e0101c44da09674e5847c68d022d900a8a
ColtonHagan <coltonmhagan@gmail.com>
2025-04-02 15:35:19 -0700

    fixing typo with prevous code

--- frontend/src/components/Loading/index.js
+++ frontend/src/components/Loading/index.js
@@ -9,8 +9,8 @@ const Loading = () => {
         <div className="spinner-container" role="alert" aria-live="polite">
             <div className="spinner" aria-hidden="true"> </div>
             <p className="spinner-message">
-                Loading...<br>
-                This may take up to 30 seconds<br>
+                Loading... <br/>
+                This may take up to 30 seconds <br/>
                 Due to free-tier API hosting backend
             </p>
         </div>
